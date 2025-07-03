9308fc82a52da4af36fd769649da56c77aecd254
Takeyuki Moriyama <3154232+t-m0riyama@users.noreply.github.com>
2025-07-03 17:03:41 +0900

    change ヘルスチェックAPIのレスポンスに共通フィールドを追加

--- app/vcenter_lookup_bridge/api/routes/healthcheck.py
+++ app/vcenter_lookup_bridge/api/routes/healthcheck.py
@@ -1,17 +1,25 @@
 from fastapi import APIRouter, Depends
 from vcenter_lookup_bridge.schemas.healthcheck_parameter import HealthcheckResponseSchema
+from vcenter_lookup_bridge.schemas.common import ApiResponse
 from vcenter_lookup_bridge.utils.logging import Logging
 from vcenter_lookup_bridge.vmware.connector import Connector
 
 
 router = APIRouter(prefix="/healthcheck", tags=["healthcheck"])
 
-@router.get('/', response_model=HealthcheckResponseSchema, description="サービスのステータスを返却します。")
+
+@router.get(
+    "/",
+    response_model=HealthcheckResponseSchema,
+    description="サービスのステータスを返却します。",
+)
 async def get_service_status(
-    content: object = Depends(Connector.get_vmware_content),
+    service_instances: object = Depends(Connector.get_service_instances),
 ):
-    vcenter_session_status = 'ok' if content else 'ng'
-    return {
-        "status": "ok",
-        "vcenter_session_status": vcenter_session_status
-    }
+    vcenter_session_status = "ok" if service_instances else "ng"
+
+    return ApiResponse.create(
+        results={"status": "ok", "vcenter_session_status": vcenter_session_status},
+        success=True,
+        message="サービスのステータスを取得しました",
+    )
--- app/vcenter_lookup_bridge/schemas/healthcheck_parameter.py
+++ app/vcenter_lookup_bridge/schemas/healthcheck_parameter.py
@@ -1,13 +1,19 @@
 from pydantic import BaseModel, Field
+from vcenter_lookup_bridge.schemas.common import ApiResponse
 
 
-class HealthcheckResponseSchema (BaseModel):
+class HealthcheckSchema(BaseModel):
     status: str = Field(
         description="サービスのステータスを示します。(ok|ng)",
         example="ok",
     )
     vcenter_session_status: str | None = Field(
-        description="vCenterへのセッションのステータスを示します。(ok|ng)",
-        example="ok"
+        description="vCenterへのセッションのステータスを示します。(ok|ng)", example="ok"
     )
     model_config = {"extra": "forbid"}
+
+
+class HealthcheckResponseSchema(ApiResponse[HealthcheckSchema]):
+    """ヘルスチェックレスポンス"""
+
+    pass
