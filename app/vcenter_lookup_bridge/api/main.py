6ff881985833ce9b5da9fbb7376861d3818eb6a7
Takeyuki Moriyama <3154232+t-m0riyama@users.noreply.github.com>
2025-07-28 17:29:08 +0900

    change ドキュメント上のエンドポイントの記述順番を変更

--- app/vcenter_lookup_bridge/api/main.py
+++ app/vcenter_lookup_bridge/api/main.py
@@ -11,11 +11,11 @@ from vcenter_lookup_bridge.api.routes import (
 )
 
 api_router = APIRouter()
-api_router.include_router(vcenters.router)
 api_router.include_router(vms.router)
 api_router.include_router(datastores.router)
 api_router.include_router(portgroups.router)
 api_router.include_router(vm_snapshots.router)
 api_router.include_router(vm_folders.router)
 api_router.include_router(healthcheck.router)
+api_router.include_router(vcenters.router)
 api_router.include_router(admins.router)
